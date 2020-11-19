950cad24d99af27e6b29f3292128a663e1152d82
Kim Sangyeob <yeob32@gmail.com>
2020-11-19 22:51:30 +0900

    #

--- spring-cloud-config/cloud-config-client/src/main/kotlin/com/example/client/config/ConfigApi.kt
+++ spring-cloud-config/cloud-config-client/src/main/kotlin/com/example/client/config/ConfigApi.kt
@@ -1,11 +1,13 @@
 package com.example.client.config
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RefreshScope
 class ConfigApi {
 
     @Autowired
